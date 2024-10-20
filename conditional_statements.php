a172bdda8d4bcb62b53f11eb6ff61907915936ad
Nouman Hameed <62693237+nouman6093@users.noreply.github.com>
2024-10-20 22:25:22 +0500

    Create conditional_statements.php

---
+++ conditional_statements.php
@@ -0,0 +1,11 @@
+<?php
+    $age = 101;
+
+    if($age >= 18){
+        echo "you may enter this site";
+    } elseif ($age == 0){
+        echo "you were just born!";
+    } else {
+        echo "you must be 18+";
+    }
+?>
