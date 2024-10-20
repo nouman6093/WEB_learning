f5b2610cbfd20c2ff45d9e208894083e59189458
Nouman Hameed <62693237+nouman6093@users.noreply.github.com>
2024-10-20 22:23:19 +0500

    Create arithmatic.php

---
+++ arithmatic.php
@@ -0,0 +1,35 @@
+<?php
+    //addition:
+    $x = 10;
+    $y = 2;
+    $z = $x + $y;
+    echo $z."<br>";
+
+    //subtraction
+    $z = $x - $y;
+    echo $z."<br>";
+
+    //multiplication:
+    $z = $x * $y;
+    echo $z."<br>";
+
+    //division:
+    $z = $x / $y;
+    echo $z."<br>";
+
+    //increment:
+    $a = 0;
+    $a++;
+    echo $a."<br>";
+
+    //decrement:
+    $b = 1;
+    $b--;
+    echo $b."<br>";
+
+    //operator precedance:
+    //()
+    //** (this is used for power i think)
+    //* / %
+    // + -
+?>
