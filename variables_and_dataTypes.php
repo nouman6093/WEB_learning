b07a93abc77234be6855a6d3bb5a0cf835883d70
Nouman Hameed <62693237+nouman6093@users.noreply.github.com>
2024-10-20 22:22:53 +0500

    Create variables_and_dataTypes.php

---
+++ variables_and_dataTypes.php
@@ -0,0 +1,15 @@
+<?php
+    $name ="Nouman";
+    $food = "pizza";
+    $email = "fake@gmail.com";
+    $age = 21;
+    $gpa = 3.29;
+    $employed = false;
+
+    echo "Hello {$name} <br>";
+    echo "You like {$food} <br>";
+    echo "Your Email is: {$email} <br>";
+    echo "You are {$age} years old <br>";
+    echo "Your GPA is {$gpa} <br>";
+    echo "Employed Status: {$employed}" //false would display nothing, true would display 1
+?>
